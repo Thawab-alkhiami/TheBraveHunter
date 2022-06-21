08405240f4e0d2a2186e21ebd86ae8c7cc5eaeb2
AreejYassin <107930794+AreejYassin@users.noreply.github.com>
2022-06-22 00:50:03 +0200

    Move the eagle toward the Gun Barrel

--- Assets/Scripts/EagleScript.cs
+++ Assets/Scripts/EagleScript.cs
@@ -4,28 +4,57 @@ using UnityEngine;
 
 public class EagleScript : MonoBehaviour
 {
+    [SerializeField] public GameObject target;
+    [SerializeField] public float _eagleSpeed = 0.5f;
     private Animator animator;
     private bool isAttacking = false;
-    [SerializeField] public GameObject target;
+    private float _distance;
+    private float _distance1;
     
     // Start is called before the first frame update
     void Start()
     {
-        animator = this.GetComponent<Animator>();
-        Debug.Log("Distance: "+Vector3.Distance(this.transform.position, target.transform.position));
+        //animator = this.GetComponent<Animator>();
+        animator = GetComponentInChildren<Animator>();
+        _distance = (target.transform.position - this.transform.position).magnitude;
+        _distance1 = Vector3.Distance(this.transform.position, target.transform.position);
+        Debug.Log("_distance: "+_distance +" _distance1: " +_distance1);
     }
 
     // Update is called once per frame
     void Update()
     {
-        //if (Vector3.Distance(this.transform.position, target.transform.position) < 10 && !isAttacking)
-        if (Input.GetButtonDown("Fire1") || Input.GetButtonDown("Jump") && !isAttacking)
-        {
-            isAttacking = true;
-            animator.SetTrigger("Attack");
-            StartCoroutine(stopAttack(1));
+        _distance = (target.transform.position - this.transform.position).magnitude;
+        //if (_distance < 3 && !isAttacking)
+        //if (Input.GetButtonDown("Fire1") || Input.GetButtonDown("Jump") 
+            //&& !isAttacking
+        //    )
+        //{
+        //transform.Rotate(50f, 0f , 0f);
+        //transform.up = target.transform.position - transform.position;
+        transform.position = Vector3.MoveTowards(transform.position, target.transform.position, _eagleSpeed * Time.time);
 
+            //isAttacking = true;
+
+        //}
+    }
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Gun") )
+        {
+            animator.SetTrigger("isDead");
+            Debug.Log("Gun Collision!, dstroy!");
+            _eagleSpeed = 0f;
+            
+            //Destroy(this.gameObject);
         }
+        
+    }
+
+    void Attack()
+    {
+        transform.Rotate(50f, 0f , 0f);
     }
     
     public IEnumerator stopAttack(float length)

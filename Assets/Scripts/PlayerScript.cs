2488e46a4873b10fc3fe544cf3dbf66572ce9cf4
AreejYassin <107930794+AreejYassin@users.noreply.github.com>
2022-06-23 02:58:36 +0200

    Create PlayerScript.cs

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    [SerializeField] public float _rotationSpeed = 50f ;
+    [SerializeField] public float _walkSpeed = 3f;
+    [SerializeField] public GameObject _bullet;
+
+    private int _lives = 3;
+    
+    // fire time delay
+    private float _nextFireTime = 0f;
+    private float _fireCoolDownTime = 0.5f;
+    
+    [SerializeField] public GameObject SpawnManager;
+   
+    void Start()
+    {
+        transform.position = new Vector3(0f, 0f, 0f);
+    }
+
+    void Update()
+    {
+        GunMovement();
+        Shoot();
+    }
+
+    public void GunMovement()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        transform.Rotate(0f, Time.deltaTime * _rotationSpeed * horizontalInput, 0f);
+
+        float verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * Time.deltaTime * _walkSpeed * verticalInput);
+        //transform.Rotate(Time.deltaTime * _speed * verticalInput * -1,0f,0f);
+    }
+
+    public void Shoot()
+    {
+        if (Input.GetButtonDown("Jump") && _nextFireTime < Time.time) //space button
+        {
+            Instantiate(_bullet, // an object to instantiate
+                    new Vector3(transform.position.x,transform.position.y - 0.035f,transform.position.z + 0.25f), //3D position
+                    transform.rotation);
+            
+            //move the bullet
+            //instantiatedBullet.velocity = transform.TransformDirection(new Vector3(0, 0,_bulletSpeed));
+            
+            //make interval between shots
+            _nextFireTime = Time.time + _fireCoolDownTime;
+            
+            //turn off collisions between the bullet and the gun
+            //Physics.IgnoreCollision(instantiatedBullet.GetComponent<Collider>(), transform.root.GetComponent<Collider>());
+        }
+    }
+
+    public void PlayerDeath()
+    {
+        _lives--;
+        Debug.Log("_lives after: "+_lives);
+        if (_lives == 0)
+        {
+            Debug.Log("player death ");
+            SpawnManager.GetComponent<SpawnManager>().onPlayerDeath();
+            Destroy(this.gameObject);
+        }
+    }
+}

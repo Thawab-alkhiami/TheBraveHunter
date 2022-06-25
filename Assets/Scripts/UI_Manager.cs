1549e4de96e23a53e8f1312911ac7f0614a4b8b4
AreejYassin <107930794+AreejYassin@users.noreply.github.com>
2022-06-26 00:12:07 +0200

    Update Lives

--- Assets/Scripts/UI_Manager.cs
+++ Assets/Scripts/UI_Manager.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class UI_Manager : MonoBehaviour
 {
+    [SerializeField] private Text _livestext;
+
+    public void UpdateLives(int health)
+    {
+        _livestext.text = "Lives: " + health;
+    }
+    
     // Start is called before the first frame update
     void Start()
     {
